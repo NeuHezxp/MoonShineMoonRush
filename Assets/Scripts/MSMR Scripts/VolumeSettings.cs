e20275d785a9507a1ec368e073ab91c93f95cdaf
Kenneth ONeal <12keniaoneal21@gmail.com>
2024-02-24 17:47:20 -0700

    audio mixer stuff

--- Assets/Scripts/MSMR Scripts/VolumeSettings.cs
+++ Assets/Scripts/MSMR Scripts/VolumeSettings.cs
@@ -6,15 +6,32 @@ using UnityEngine.UI;
 
 public class VolumeSettings : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private AudioMixer masterMixer;
+    [SerializeField] private Slider musicSlider;
+    [SerializeField] private Slider SFXSlider;
+
+    private void Start()
     {
-        
+        SetMusicVolume(); // makes audio compatible
+        SetSFXVolume();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetMusicVolume()
     {
-        
+        float volume = musicSlider.value;
+        masterMixer.SetFloat("Music", Mathf.Log10(volume)*20);
+        PlayerPrefs.SetFloat("MusicVolume", volume);
+    }
+    public void SetSFXVolume()
+    {
+        float volume = SFXSlider.value;
+        masterMixer.SetFloat("Music", Mathf.Log10(volume) * 20);
+    }
+
+    private void LoadVolume()
+    {
+        musicSlider.value = PlayerPrefs.GetFloat("MusicVolume");
+
+        SetMusicVolume();
     }
 }
